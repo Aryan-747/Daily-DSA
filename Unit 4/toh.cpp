33ef91ae2fb3082928593deb7c2034cfe4984bd3
Aryan Salaria <arayn.32@gmail.com>
2023-10-10 00:43:15 +0530

    TOH

--- Unit 4/toh.cpp
+++ Unit 4/toh.cpp
@@ -2,9 +2,40 @@
 
 using namespace std;
 
-int main()
+
+void TOH(int n, char source , char helper , char destination)
 {
 
-    
+    if(n == 1)
+    {
+        cout << "Move " << n << " disk from " << source << " to " << destination << endl;
+        return ;
+    }
 
+    TOH(n-1,source,destination,helper);
+    cout << "Move " << n << " disk from " << source << " to " << destination << endl;
+    TOH(n-1,helper,source,destination);
+
+}
+
+int main()
+{
+    int n;
+    char source;
+    char helper;
+    char destination;
+
+    cout << "Enter Number of Disks: ";
+    cin >> n;
+
+    cout << "Enter name of Source: ";
+    cin >> source;
+
+    cout << "Enter name of Helper: ";
+    cin >> helper;
+
+    cout << "Etner name of Destination: ";
+    cin >> destination;
+
+    TOH(n,source,helper,destination);
 }
