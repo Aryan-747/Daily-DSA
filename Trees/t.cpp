1a98c7724e26f2d8515955805346634b4c6854fd
Aryan Salaria <arayn.32@gmail.com>
2023-10-30 23:22:06 +0530

    Trees 2

---
+++ Trees/t.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+using namespace std;
+
+class Tnode
+{  
+    public:
+
+    int data;
+    Tnode *right;
+    Tnode *left;
+
+    Tnode(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+};
+
+
+Tnode* buildtree(Tnode* root)
+{   
+    int data;
+    cout << "Enter the Data: ";
+    cin >> data;
+
+    root = new Tnode(data);
+
+    if(data == -1)
+    {
+        return NULL;
+    }
+
+    cout << "Enter data to insert in left of " << data << endl;
+    root->left = buildtree(root->left);
+
+    cout << "Enter data to insert in right of " << data << endl;
+    root->right = buildtree(root->right);
+
+    return root; 
+
+}
+
+void inorder(Tnode *root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+
+int main()
+{
+    Tnode *root = NULL;
+
+    root = buildtree(root);
+
+    inorder(root);
+
+
+}
